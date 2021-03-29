d1d57a4c84a89ce67650acc00ef47fd0e7fab832
rachna112-gautam <rachna01121999@gmail.com>
2021-03-29 22:18:09 +0530

    ..

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -9,52 +9,23 @@ import { connect } from "react-redux";
 import { Row, Col } from "reactstrap";
 
 const Dashboard = (props) => {
-<<<<<<< HEAD
-    const [contract, setContract] = useState();
-    const [poolsPrice, setPoolsPrice] = useState([]);
-
-
-    // console.log("props", props)
-    useEffect(() => {
-        setContract(contract);
-        dollars();
-
-    }, [props.contract])
-
-
-    const dollars = async () => {
-        if (!props.contract || !props.account) {
-            alert("contract not loaded");
-            return;
-        }
-        let dollars = (await props.contract.methods.dollars().call()).toNumber() / 10 ** 6;
-        console.log("dollar", dollars);
-        let poolPrice = [];
-        for (let i = 0; i < 20; i++) {
-            let res = (await props.contract.poolsPrice(i).call()).toNumber();
-            poolPrice[i] = (res);
-        }
-        setPoolsPrice(poolPrice);
-        console.log("pool price", poolPrice)
-=======
   const [contract, setContract] = useState();
   const [poolsPrice, setPoolsPrice] = useState([]);
   // const [personalData, setPersonalData] = useState();
 
   console.log("props", props);
-  
+
   useEffect(() => {
     setContract(contract);
     dollars();
     console.log("dashboard use effect")
-  },[props.account]);
-  
+  }, [props.account]);
+
 
   const dollars = async () => {
     if (!props.contract || !props.account) {
       alert("contract not loaded");
       return;
->>>>>>> 972595cf1538cf263e90825b7290e155b0df3429
     }
     let dollars =
       (await props.contract.methods.dollars().call()).toNumber() / 10 ** 6;
@@ -88,40 +59,6 @@ const Dashboard = (props) => {
       return;
     }
 
-<<<<<<< HEAD
-    return (
-        <div className="dashboard">
-
-            <Header />
-
-            <div className="row flex-wrap-reverse">
-                <div className="col-lg-8">
-                    <Left data={props.personalData} />
-                </div>
-                <div className="col-lg-4">
-
-                    <Right />
-                </div>
-            </div>
-            <Row>
-                <Col xs="6" sm="6" lg="6">
-                    <button className="btn more-btn" onClick={() => {
-                        upgradePool();
-                    }}>Upgrade Pool</button>
-                </Col>
-                <Col xs="6" sm="6" lg="6">
-                    <button className="btn more-btn" onClick={() => {
-                        enter();
-                    }}> Enter</button>
-                </Col>
-            </Row>
-
-            <div className="row ">
-                <div className="col-lg-12">
-                    <Table />
-                </div>
-            </div>
-=======
     if (props.personalData.isExist) {
       alert("user already exists");
       return;
@@ -141,10 +78,9 @@ const Dashboard = (props) => {
         </div>
         <div className="col-lg-4">
           <Right />
->>>>>>> 972595cf1538cf263e90825b7290e155b0df3429
         </div>
       </div>
-      {/* <Row>
+      <Row>
         <Col xs="6" sm="6" lg="6">
           <button
             className="btn more-btn"
@@ -166,7 +102,7 @@ const Dashboard = (props) => {
             Enter
           </button>
         </Col>
-      </Row> */}
+      </Row>
 
       <div className="row ">
         <div className="col-lg-12">
