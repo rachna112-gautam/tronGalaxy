7464c39e1f12a33d4cda34a21e563b6a1f2f1359
Mosajjid <mosajjid.khan@gmail.com>
2021-04-18 22:34:37 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css';
 import Particles from 'react-particles-js';
 import LandingPage from './Components/LandingPage/LandingPage';
@@ -7,36 +8,35 @@ import { connect } from 'react-redux';
 import Blockchain from './BlockchainProvider/blockchain';
 
 function App(props) {
-	return(
-<div>
-				<Particles
-					className="particles"
-					params={{
-						particles: {
-							number: {
-								value: 50
-							},
-							size: {
-								value: 3
-							}
-						},
-						interactivity: {
-							events: {
-								onhover: {
-									enable: true,
-									mode: 'repulse'
-								}
-							}
-						}
-					}}
-				/>
-				<Router>
-				{/* <Route path="/" exact component={LandingPage} /> */}
-				<Route path="/" exact component={Dashboard} />
-			
-		</Router>
-		</div>
-	);
+	return (
+    <div>
+      <Particles
+        className="particles"
+        params={{
+          particles: {
+            number: {
+              value: 50,
+            },
+            size: {
+              value: 3,
+            },
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+          },
+        }}
+      />
+      <Router>
+        {/* <Route path="/dashboard" exact component={Dashboard} /> */}
+        <Route path="/" exact component={LandingPage} />
+      </Router>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
